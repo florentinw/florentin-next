e0ff8ddaf0c107e6ae59964cd1e530704f487253
tobimori <tobias@moeritz.cc>
2020-05-15 14:56:01 +0200

    💄

--- components/CustomLink.js
+++ components/CustomLink.js
@@ -8,7 +8,7 @@ const StyledLink = styled.a`
   text-decoration: none;
   line-height: 1;
   display: inline;
-  padding: ${props => (props.noBorder ? '0px' : '0 2px 0.15em 2px')};
+  padding: ${props => (props.noBorder ? '0px' : '0 2px 0.0.8em 2px')};
   border-bottom: ${props => (props.noBorder ? 'none' : '0.15em solid var(--lowerContrast)')};
 `
 
