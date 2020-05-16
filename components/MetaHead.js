ffccbea2d94f79c0439509f4e220b5975c576f59
tobimori <tobias@moeritz.cc>
2020-05-16 10:06:09 +0200

    🐛

--- components/MetaHead.js
+++ components/MetaHead.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 
 const MetaHead = ({ title, description, image, slug, children, isHome }) => (
   <Head>
+    <meta charset='utf-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
     <title>{isHome ? title : `${title} | Florentin Walter — Freelance Graphic Designer`}</title>
     <meta name='title' content={isHome ? title : `${title} | Florentin Walter — Freelance Graphic Designer`} />
     <meta name='description' content={description} />
@@ -23,6 +25,9 @@ const MetaHead = ({ title, description, image, slug, children, isHome }) => (
     <meta name='twitter:site' content='@florentin' />
     <meta name='twitter:creator' content='@florentin' />
     <meta name='twitter:card' content='summary_large_image' />
+    <link rel='shortcut icon' href='https://florentin.design/favicon.ico' type='image/x-icon' />
+    <meta property='og:logo' content='https://florentin.design/logo-400.jpg' size='400x400' />
+    <meta property='og:logo' content='https://florentin.design/logo-128.jpg' ize='128x128' />
     {children}
   </Head>
 )
