c9c3ee9e2ceb6ec1127b1dacaac3b2e48730016a
Florentin <email@florentinwalter.de>
2020-07-08 15:18:44 +0200

    Update ImageWithContainer.js

--- components/ImageWithContainer.js
+++ components/ImageWithContainer.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { useElementScroll, useViewportScroll, useTransform } from 'framer-motion'
+import { useMotionValue, useViewportScroll, useTransform } from 'framer-motion'
 import { useRef, useEffect } from 'react'
 
 import { Container, Image } from '.'
@@ -19,26 +19,51 @@ const StyledContainer = styled(Container)`
   margin: 20px auto;
   height: 400px;
   overflow: hidden;
+  border-radius: 5px;
   @media screen and (max-width: 768px) {
     min-height: 60vh;
   }
 `
 
+const percentageSeen = (element) => {
+  // Get the relevant measurements and positions
+  const viewportHeight = window.innerHeight
+  const scrollTop = window.scrollY
+  const elementOffsetTop = element.offsetTop
+  const elementHeight = element.offsetHeight
+
+  // Calculate percentage of the element that's been seen
+  const distance = scrollTop + viewportHeight - elementOffsetTop
+  const percentage = Math.round(
+    distance / ((viewportHeight + elementHeight) / 100)
+  )
+
+  // Restrict the range to between 0 and 100
+  return Math.min(100, Math.max(0, percentage))
+}
+
+
 const ImageWithContainer = (props) => {
   const ref = useRef()
-  const { scrollYProgress } = useElementScroll(ref)
-  //  const { scrollYProgress } = useViewportScroll()
-  const scale = useTransform(scrollYProgress, [0, 1], [0.4, 2])
+
+  const { scrollYProgress } = useViewportScroll()
+  const scale = useMotionValue(1)
 
   useEffect(() => {
-    console.log({ scaleLog: scale })
-    console.log({ scrollYProgressLog: scrollYProgress })
-  }, [scale, scrollYProgress])
+    const unsubscribeScrollYProgress = scrollYProgress.onChange(() => {
+      const element = ref.current
+      if (element) scale.set(0.2 * percentageSeen(element) / 100 + 1)
+    })
+    return () => {
+      unsubscribeScrollYProgress()
+    }
+  }, [])
 
   return (
     <StyledContainer ref={ref}>
       <StyledImage
         style={{ scale }}
+        transition={{ delay: 0, ease: 'easeOut' }}
         {...props}
       />
     </StyledContainer>
@@ -47,16 +72,14 @@ const ImageWithContainer = (props) => {
 
 export default ImageWithContainer
 
-
 /*
 Styled container
       initial={{ opacity: 0, y: 40 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{
         delay: 0.5, duration: 1, ease: [0.6, -0.05, 0.01, 0.99]
-      }}*/
-
+      }} */
 
 /*
-        transition={{ type: 'tween' }}
+
 */
